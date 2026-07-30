c157fb970e5aec2273b51067af41c396e06683c8
Rahul kumar <rkum6094@gmail.com>
2026-07-30 15:33:43 +0530

    all change

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,8 +4,7 @@ public class WelcomeToJava {
        System.out.println("Welcome to the Java Track");
     System.out.println("I already know how to build programing logic.");
     System.out.println("Now i am Learning how to express that logic using java");
-
-
+    System.out.println("today i learn git ");
 
     }
 }
