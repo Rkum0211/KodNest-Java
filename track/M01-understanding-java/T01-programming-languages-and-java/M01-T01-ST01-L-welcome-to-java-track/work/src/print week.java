b172a04a8d33155670302e0ce2429218bbf95a2b
Rahul kumar <rkum6094@gmail.com>
2026-08-04 11:48:15 +0530

    Practice

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/print week.java
@@ -0,0 +1,11 @@
+public class print week {
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.println("tuesday");
+        System.out.print("Wednesday\n");
+        System.out.println("Thursday");
+        System.out.print("Friday\n");
+        System.out.println("saturday");
+        System.out.print("sunday\n");
+    }
+}
