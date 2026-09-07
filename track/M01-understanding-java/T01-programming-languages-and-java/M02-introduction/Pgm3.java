221934dada757778cf0c2e00529d31f4069117a3
Rahul kumar <rkum6094@gmail.com>
2026-09-07 17:29:51 +0530

    add new programe

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M02-introduction/Pgm3.java
@@ -0,0 +1,8 @@
+public class Pgm3 {
+    public static void main(java.lang.String[] args) {
+        java.lang.String s3 = new java.lang.String("Java");
+        s3.concat("is fun");
+        System.out.println(s3);
+    }
+}
+
