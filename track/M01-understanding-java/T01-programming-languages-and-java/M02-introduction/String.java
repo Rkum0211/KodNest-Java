23ebe0785da84cf4914c1db96ba1446f5278302b
Rahul kumar <rkum6094@gmail.com>
2026-09-07 17:22:25 +0530

    add new programe

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M02-introduction/String.java
@@ -0,0 +1,12 @@
+public class String {
+    public static void main(java.lang.String[] args) {
+        java.lang.String s1 = "Java";
+        java.lang.String s2 = "Java";
+        if (s1 == s2) {
+            System.out.println("Ref are equal");
+        } else {
+            System.out.println("Ref are not equal");
+        }
+    }
+}
+
---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M02-introduction/StudentApplication02.java
@@ -0,0 +1,22 @@
+public class StudentApplication02 {
+    String name; 
+    int age;
+    double height;
+    Student(String name, int age' double height)
+    {
+    this.name = name;
+    this.name = age;
+    this.height = height;
+    }
+    void display()
+}
+    System.out.println("name");
+    System.out.println(this.name);
+    System.out.println("age");
+    System.out.println(this.age);
+    System.out.println("height");
+    System.out.println(this.height);
+}
+class StudentApplication02{
+    public static void main(String args [])
+}
