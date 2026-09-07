232e616dde0633dc3a092fb2d5c54f2e2efd653f
Rahul kumar <rkum6094@gmail.com>
2026-09-07 17:27:03 +0530

    add new programe

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M02-introduction/Pgm2.java
@@ -0,0 +1,12 @@
+public class Pgm2 {
+    public static void main(java.lang.String[] args) {
+        java.lang.String s3 = new java.lang.String("Java");
+        java.lang.String s4 = new java.lang.String("Java");
+        if (s3 == s4) {
+            System.out.println("REF is equal");
+        } else {
+            System.out.println("REF is not equal");
+        }
+    }
+}
+
