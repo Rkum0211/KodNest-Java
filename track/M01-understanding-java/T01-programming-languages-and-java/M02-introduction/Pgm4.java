dac23a3ea0747a6725e02e604a6edc5c66f45ade
Rahul kumar <rkum6094@gmail.com>
2026-09-08 16:55:40 +0530

    add new programe

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M02-introduction/Pgm4.java
@@ -0,0 +1,17 @@
+public class Pgm4 {
+    public static void main(java.lang.String[] args) {
+    
+        java.lang.String s1 = "Java";
+        java.lang.String s2 = "JaVa";
+        if(s1 == s2){
+            System.out.println("Ref is equal");
+        }
+        else{
+            System.out.println("Ref is not equal");
+        }
+
+        
+    }
+    
+}
+
