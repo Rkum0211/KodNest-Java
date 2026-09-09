e4f1c7777f9511402b55fed35987944fa88a994d
Rahul kumar <rkum6094@gmail.com>
2026-09-09 16:48:27 +0530

    add new programe

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M02-introduction/StringComparision3.java
@@ -0,0 +1,10 @@
+public class StringComparision3 {
+    public static void main(java.lang.String[] args) {
+        java.lang.String s1 = "Java";
+        java.lang.String res = s1.toUpperCase();
+        System.out.println(res);
+        
+        java.lang.String res1 = s1.toLowerCase();
+        System.out.println(res1);
+    }
+}
