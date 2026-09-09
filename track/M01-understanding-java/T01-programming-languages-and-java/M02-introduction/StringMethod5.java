ea25390ad0f6b0ea474834da35c1825ab1c469b6
Rahul kumar <rkum6094@gmail.com>
2026-09-09 16:55:49 +0530

    add new programe

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M02-introduction/StringMethod5.java
@@ -0,0 +1,12 @@
+public class StringMethod5 {
+    public static void main(java.lang.String[] args) {
+        java.lang.String s1 = "Rahul";
+        java.lang.String s2 = "Ravi";
+        java.lang.String s3 = s1 + s2;
+        System.out.println(s3);
+        java.lang.String t1 = new java.lang.String("Bhanu");
+        java.lang.String t2 = new java.lang.String("Rishab");
+        java.lang.String res = t1.concat(t2);
+        System.out.println(res);
+    }
+}
